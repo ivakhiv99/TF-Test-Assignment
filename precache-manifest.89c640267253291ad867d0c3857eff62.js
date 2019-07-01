43946db9be9101ab4eca71dec62f33b835659ff0
Vitaliy <ivahiv99@gmail.com>
2019-07-02 02:33:06 +0300

    Updates

---
+++ precache-manifest.89c640267253291ad867d0c3857eff62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "02eb4bc5a4169c1a77b574cc6d02c178",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b913ad2cb5c01ece604",
+    "url": "/static/css/main.c84a9830.chunk.css"
+  },
+  {
+    "revision": "aa3690354121ddd4ca1a",
+    "url": "/static/js/2.1197b116.chunk.js"
+  },
+  {
+    "revision": "8b913ad2cb5c01ece604",
+    "url": "/static/js/main.689e4c9f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
